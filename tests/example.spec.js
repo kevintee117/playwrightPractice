200ab6353f59e121ed7b130fb682c09ca7f7d6c1
Kevin Tee <kevintee@Kevins-MacBook-Neo.local>
2026-05-17 16:50:21 -0700

    removed example.spec.js

--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('successful login', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  await expect(page).toHaveURL(/inventory/);
-});
